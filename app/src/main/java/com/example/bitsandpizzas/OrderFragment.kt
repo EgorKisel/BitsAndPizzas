89ad3487ac8d771de28d09734a077a8a27cc78c5
Egor Kisel <kisel1989@mail.ru>
2023-09-11 18:04:48 +0300

    Toolbar скрывается при прокрутке

---
+++ app/src/main/java/com/example/bitsandpizzas/OrderFragment.kt
@@ -0,0 +1,31 @@
+package com.example.bitsandpizzas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+
+class OrderFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_order, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            OrderFragment()
+    }
+}
