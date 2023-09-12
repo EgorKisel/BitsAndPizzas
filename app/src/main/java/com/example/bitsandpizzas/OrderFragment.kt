689490fbb09daf9865721633c0424806db4210fc
Egor Kisel <kisel1989@mail.ru>
2023-09-12 18:44:45 +0300

    FloatingActionButton + setOnClickListener + Toast + Snackbar

--- app/src/main/java/com/example/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/com/example/bitsandpizzas/OrderFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class OrderFragment : Fragment() {
 
@@ -19,8 +24,32 @@ class OrderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizzaGroup)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                Toast.makeText(
+                    activity,
+                    "You need to choose a pizza type",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                var text = (when (pizzaType) {
+                    R.id.radioDiavolo -> "Diavolo pizza"
+                    else -> "Funghi pizza"
+                })
+                val parmesan = view.findViewById<Chip>(R.id.parmesan)
+                text += if (parmesan.isChecked) ", extra parmesan" else ""
+                val chiliOil = view.findViewById<Chip>(R.id.chiliOil)
+                text += if (chiliOil.isChecked) ", extra chili oil" else ""
+                Snackbar.make(fab, text, Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 
     companion object {
